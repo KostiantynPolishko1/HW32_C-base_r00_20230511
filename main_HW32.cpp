fb8a32d30329b7a4d8c15c4858c49c4854afa420
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-05-20 01:21:02 +0300

    8 Deleted description of tasks from main_HW32.cpp

--- main_HW32.cpp
+++ main_HW32.cpp
@@ -5,11 +5,6 @@
 #include "checkV.h"
 using namespace std;
 
-//Task1
-//Write a header file "math_utils.h" that contains the following function declarations:
-//int sum(int a, int b)			: function receive two int values and return them sum.
-//int subtract(int a, int b)	: function receive two int values and return them substraction.
-//int multiply(int a, int b)	: function receive two int values and return them multiplication.
 void printArr(int arr[], int &size)
 {
 	for (int i = 0; i < size; i++)
@@ -32,13 +27,6 @@ void Task1()
 
 }
 
-//Task2
-//Write a header file "array_utils.h", which has declarations of the next functions
-// for working with arrays:
-
-//int findMax(int arr[], int size) : function takes the array of int values and his size and return max value in array
-//int findMin(int arr[], int size) : function takes the array of int values and his size and return min value in array
-//double calculateAverage(int arr[], int size) : function takes the array of int values and his size and return simple average in array
 void Task2(int arr[], int& size)
 {
 	cout << "\n Array: ";
@@ -50,11 +38,6 @@ void Task2(int arr[], int& size)
 	
 }
 
-//Task3
-//Write the header file "sorting_utils.h", which has declaretion of next function for sorting:
-//void bubbleSort(int arr[], int size): sorting array of int values by algoritm bubble in ascending order;
-//void selectionSort(int arr[], int size): sorting array of int values by algoritm select in ascending order;
-//void insertionSort(int arr[], int size): sorting array of int values by algoritm insert in ascending order;
 void Task3(int arr[], int &size)
 {
 	int n = 0;
@@ -115,7 +98,7 @@ restart0:
 		void(*operation[2])(int[], int&) = { Task2, Task3 };
 		operation[n](arr, size);
 	}
-	
+
 
 	return 0;
 }
